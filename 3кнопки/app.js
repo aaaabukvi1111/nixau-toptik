8398f743de042365e822c6c67acd8d08e7015a92
user444 <104263986+nouname0@users.noreply.github.com>
2023-02-04 18:50:11 +0300

    Add files via upload

---
+++ 3кнопки/app.js
@@ -0,0 +1,13 @@
+function first() {
+    // Про 10+50 кнопка
+    alert(`${505+505}`)
+    }
+    function second() {
+    // prompt() -  можем записать данные в переменную
+  name = prompt(alert(name))
+    }
+    function third() {
+    // Вставить в элемент HTML с id 'zp' вставить тысячу
+    document.getElementById('zp').innerHTML = '1$';
+    }
+    
